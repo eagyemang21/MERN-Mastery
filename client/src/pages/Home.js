e553c2811755b3dfc5b3e546373fe3dc72504670
eagyemang21 <elijahagyemang9@gmail.com>
2023-02-05 22:15:42 -0500

    was able to add ReactRouter to it!

---
+++ client/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Home = () =>{
+    return(
+        <div>
+            <h2>Home</h2>
+            <p>What's up</p>
+            <p>Giiiiirl</p>
+            <div>
+                <ol>
+                    <li>You</li>
+                    <li>can</li>
+                    <li>kiss</li>
+                    <li>my</li>
+                    <li>behind</li>
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
